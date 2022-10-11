ac87f234b5e4e7e6a3c732e2dbebcb7d1d3218fc
gracesianipar <114061711+gracesianipar@users.noreply.github.com>
2022-10-11 15:05:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+let show = function(data) {
+    let arr = JSON.parse(data)
+    arr.forEach(result => {
+        let tabel = document.getElementById("tabel")
+        tabel.innerHTML = tabel.innerHTML + `
+            <tr>
+                <td><b> ${result.id} </b></td>
+                <td> ${result.name} </td>
+                <td> ${result.username} </td>
+                <td> ${result.email} </td>
+                <td> ${result.address.city} </td>
+            </tr>
+        `
+    })
+
+}
+
+let load = function() {
+    let link = "https://jsonplaceholder.typicode.com/users"
+
+    try {
+        let xhr = new XMLHttpRequest()
+        if (!xhr) return -1
+
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                show(xhr.responseText)
+            }
+        }
+        xhr.open('GET', link)
+        xhr.send()
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+load()
